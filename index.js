34086f455f80e8becd3ef80efecb57bc066c23fc
sneht <sneh.frontendarmy@gmail.com>
2023-10-19 17:39:31 +0530

    [cors FIX]

--- index.js
+++ index.js
@@ -40,7 +40,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chat-app-st.netlify.app/",
+    // "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
